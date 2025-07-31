1ecefb1f4101ba7c5cd362ca0923585636100ff1
sohitpatel259 <sohitpatel359@gmail.com>
2025-07-31 22:46:25 +0530

    start

---
+++ encodegenerater.py
@@ -0,0 +1,37 @@
+import cv2
+import face_recognition
+import pickle
+import os
+
+# importing images list
+folderpath = "images"
+pathlist = os.listdir(folderpath)
+# print(pathlist)
+imglist = []
+studentids = []
+for path in pathlist:
+    imglist.append(cv2.imread(os.path.join(folderpath,path))) #folder of modes   
+    studentids.append( os.path.splitext(path)[0]) #remove .jpg from image
+    # print(path)
+    # print( os.path.splitext(path)[0])
+
+print(studentids)
+
+
+def findencoding(imglist):
+    encodelist=[]
+    for img in imglist:
+        img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        encode=face_recognition.face_encodings(img)[0]
+        encodelist.append(encode)
+
+    return encodelist
+print("Encoding begin.... ")
+encodelistknown = findencoding(imglist)
+encodelistknownIds = [encodelistknown,studentids]
+print("Encoding Complete ")
+
+file = open("EncodeFileFaceRecognition.p",'wb')
+pickle.dump(encodelistknownIds,file)
+file.close()
+print("file saved")
---
+++ main.py
@@ -0,0 +1,56 @@
+import cv2
+import os
+import pickle
+import face_recognition
+
+cap=cv2.VideoCapture(0)
+cap.set(3,640)  # Set width
+cap.set(4,480)  # Set height
+
+imgBackground = cv2.imread("resources/background.png")
+
+# importing mode list
+foldermodepath = "resources/folders"
+modepathlist = os.listdir(foldermodepath)
+imgmodellist = []
+for path in modepathlist:
+    imgmodellist.append(cv2.imread(os.path.join(foldermodepath,path)))
+
+# print(len(imgmodellist))
+
+
+#load encodings file
+print("load encode file... ")
+file = open("EncodefileFaceRecognition.p",'rb')
+encodelistknownIds = pickle.load(file)
+file.close()
+encodelistknown,studentids = encodelistknownIds
+#print(studentids)
+print("Encoded file loaded")
+
+while True:
+    success, img=cap.read()
+
+    imgsmall = cv2.resize(img,(0,0),None, 0.25, 0.25) #resize the image
+    imgsmall = cv2.cvtColor(imgsmall,cv2.COLOR_BGR2RGB)
+
+    facecurrentFrame = face_recognition.face_locations(imgsmall)
+
+
+    #encode for new life image to validate the saved and live image
+    enccodecurrFrame = face_recognition.face_encodings(imgsmall,facecurrentFrame)
+
+    imgBackground[178:178+480 , 61:61+640] = img
+    imgBackground[46:46+666 , 810:810+444] = imgmodellist[1]
+
+    for encodeface, faceloc in zip(enccodecurrFrame,facecurrentFrame):
+        matches = face_recognition.compare_faces(encodelistknown,encodeface)
+        facedistance = face_recognition.face_distance(encodelistknown,encodeface)
+        # print("matches",matches)
+        print("matches", [bool(match) for match in matches]) #i use this because i want to remove np.true , np.false to ture and false
+        print("facedistance",facedistance)
+
+    cv2.imshow("webcam", img)
+    cv2.imshow("Face Attendence",imgBackground)
+    cv2.waitKey(1)
+
